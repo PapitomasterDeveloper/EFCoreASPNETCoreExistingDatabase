11775657a9c0441d09ce3014b4b97d418a2a23be
papitomasterdeveloper <papitomasterdeveloper@gmail.com>
2019-01-30 11:06:52 -0600

    Consumed from a datasource DB via Reverse Engineer with CRUD operations

---
+++ EFCoreASPNETExistingDatabase/Models/Blog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFCoreASPNETExistingDatabase.Models
+{
+    //Entity classes that represent the data you will be querying and saving
+    public partial class Blog
+    {
+        public Blog()
+        {
+            Post = new HashSet<Post>();
+        }
+
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+
+        public virtual ICollection<Post> Post { get; set; }
+    }
+}
---
+++ EFCoreASPNETExistingDatabase/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFCoreASPNETExistingDatabase.Models
+{
+    //Entity classes that represent the data you will be querying and saving
+    public partial class Post
+    {
+        public int PostId { get; set; }
+        public int BlogId { get; set; }
+        public string Content { get; set; }
+        public string Title { get; set; }
+
+        public virtual Blog Blog { get; set; }
+    }
+}
